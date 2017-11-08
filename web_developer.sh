78c536184f7bf24a916154467d8ca05739f9c680
theapache64-charlie <theapache64@gmail.com>
2017-11-08 15:05:21 +0530

    .backup

--- web_developer.sh
+++ web_developer.sh
@@ -4,27 +4,37 @@ echo "###############################################"
 
 
 echo "Installing sublime repo..."
-# sublime
 sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
 
 echo "Installing java repo..."
-# java
 sudo add-apt-repository -y ppa:webupd8team/java
+
+echo "Installing netspeed indicator repo"
+sudo add-apt-repository ppa:nilarimogard/webupd8
 
 sudo apt-get -y --force-yes update
 
 echo "Installing mysql"
 sudo apt-get -y install mysql-server
+
 echo "Installing php"
 sudo apt-get -y install phpmyadmin
+
 echo "Installing sublime"
 sudo apt-get -y install sublime-text-installer
+
 echo "Installing java"
 sudo apt-get -y install oracle-java8-installer oracle-java8-set-default
+
 echo "Installing putty"
 sudo apt-get -y install putty
+
 echo "Installing filezilla"
 sudo apt-get -y install filezilla
+
+echo "Installing netspeed indicator"
+sudo apt-get install indicator-netspeed
+
 
 echo "Installing phpstorm"
 #installing phpstorm
@@ -39,6 +49,8 @@ echo "Installing google chrome"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt-get install -f
+
+
 
 
 echo "###############################################"
