d01bb659f7163ab225f86b31c67f53b25d69c06c
theapache64 <theapache64@gmail.com>
2017-11-10 12:12:56 +0530

    Update web_developer.sh

--- web_developer.sh
+++ web_developer.sh
@@ -45,7 +45,7 @@ echo "Installing phpstorm"
 wget https://download.jetbrains.com/webide/PhpStorm-2017.2.4.tar.gz -O storm.tar.gz
 tar -xvf storm.tar.gz
 sudo mv PhpStorm-* /opt/
-sudo sh /opt/PhpStorm-*/bin/phpstorm.sh
+sh /opt/PhpStorm-*/bin/phpstorm.sh
 #Disabling licence check
 echo '0.0.0.0 accounts.jetbrains.com' | sudo tee --append /etc/hosts > /dev/null
 
